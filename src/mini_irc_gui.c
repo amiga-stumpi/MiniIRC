fa27f52e745c7ad376a594229572c75e1f6c9e2f
Madzel <stumpi@userzap.de>
2026-06-02 16:18:58 +0000

    Close MiniIRC channels on reconnect

--- src/mini_irc_gui.c
+++ src/mini_irc_gui.c
@@ -1266,9 +1266,38 @@ static int send_registration(void)
     return mini_irc_session_send_line(&g_gui.session, g_send_buf);
 }
 
+static void reset_channel_tabs(void)
+{
+    g_tab_count = 1;
+    g_active_tab = 0;
+    g_tabs[0].user_count = 0;
+    g_tabs[0].names_receiving = 0;
+    g_leave_w = 0;
+    g_leave_h = 0;
+}
+
+static void part_open_channels(void)
+{
+    int i;
+    int pos;
+
+    if (!g_gui.connected)
+        return;
+    for (i = 1; i < g_tab_count; ++i) {
+        pos = 0;
+        g_send_buf[0] = 0;
+        if (!append_text(g_send_buf, &pos, sizeof(g_send_buf), "PART "))
+            continue;
+        if (!append_text(g_send_buf, &pos, sizeof(g_send_buf), g_tabs[i].name))
+            continue;
+        mini_irc_session_send_line(&g_gui.session, g_send_buf);
+    }
+}
+
 static void disconnect_irc(const char *reason)
 {
     debug_log("DISCONNECT", reason ? reason : "Disconnected");
+    part_open_channels();
     if (g_gui.connected)
         mini_irc_session_send_line(&g_gui.session, "QUIT :bye");
     if (g_gui.fd >= 0 && g_gui.socket_base)
@@ -1276,6 +1305,7 @@ static void disconnect_irc(const char *reason)
     g_gui.fd = -1;
     g_gui.connected = 0;
     g_gui.sock_ctx.fd = -1;
+    reset_channel_tabs();
     tab_append(0, reason ? reason : "Disconnected");
     draw_channel_list();
     draw_user_list();
